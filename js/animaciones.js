ff0d1631d49c0d100684b1fe30d1ff736a1748de
Eljuancho871 <juandiegoramirez098@gmail.com>
2023-05-21 21:36:06 -0500

    feat: :sparkles: animaciones
    animaciones terminadas

---
+++ js/animaciones.js
@@ -0,0 +1,70 @@
+$capa = document.querySelector("#capa");
+
+
+document.addEventListener("click", (e) => {
+
+    console.log(e.target.className)
+    if(e.target.className[0] == "d"){
+
+        $capa.style.width = "100%";
+        $capa.style.height = "100vh";
+        $capa.style.borderRadius = "0 0 0% 0";
+        document.querySelector(".windows_promp").style.zIndex = "100"
+
+        setTimeout(() => {
+            document.querySelector(".windows_promp").style.opacity = "1"
+        }, 500);
+    }
+    
+    if(e.target.id == "exit"){
+        
+        $capa.style.width = "0%";
+        $capa.style.height = "0vh";
+        $capa.style.borderRadius = "0 0 50% 0";
+        
+        document.querySelector(".windows_promp").style.opacity = "0"
+        document.querySelector(".windows_promp").style.zIndex = "-100"
+    }
+})
+
+let animation_shadow;
+let animation_temblor = document.querySelectorAll("[data-temblor]");
+let animation_shadow_all = document.querySelectorAll(".show_red");
+
+document.addEventListener("mouseover", e => {
+
+    if(e.target.id == "sismos_true"){
+
+        try{
+            animation_temblor.forEach(el => {
+                el.classList.add("temblor");
+            })
+    
+            animation_shadow_all[e.target.dataset.showNumber].style.boxShadow = "0px 0px 10px red";
+            animation_shadow = animation_shadow_all[e.target.dataset.showNumber];
+        }
+        catch(err){
+
+        }
+        
+    }else{
+
+        try{
+            animation_shadow_all.forEach(el => {
+
+                el.style.boxShadow = "none";
+            })
+            animation_temblor.forEach(el => {
+                el.classList.remove("temblor");
+            })
+        }
+        catch(err){
+
+        }
+    }
+})
+
+// document.addEventListener("mouseout", e => {  temblor
+
+//     console.log(e.target)
+// })
